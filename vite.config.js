7c2d47a0dcd2cef64971ece6dc80a4b2ce2c1761
Marcos Alves <marcosandrearrudaalves@gmail.com>
2026-06-02 11:22:44 +0100

    fix: mover propriedade base para a raiz das configuracoes do vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/web_project_around_react/",
   server: {
     port: 3000,
-    base: "/web_project_around_react/",
   },
 });
